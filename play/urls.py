e6df937a3075ac43ab35b463b493c08c9d3cd373
ulysses <ywj159676@gmail.com>
2019-07-18 15:10:52 +0000

    edit play_urls

--- play/urls.py
+++ play/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:song_id>.html',views.playView,name='play'),
+    path('download/<int:song_id>.html',views.downloadView,name='download')
 ]
